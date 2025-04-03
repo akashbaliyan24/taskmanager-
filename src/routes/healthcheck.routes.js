a092df5ce9c3d1bded46cbabdee08f57a0aed59a
Akash <akashbaliya24@gmail.com>
2025-04-04 01:22:01 +0530

    Updated some files inside routes directery

--- src/routes/healthcheck.routes.js
+++ src/routes/healthcheck.routes.js
@@ -1,10 +1,7 @@
-import {Router} from "express"
+import { Router } from "express";
 import { healthCheck } from "../controllers/healthcheck.controller.js";
-import {registerUser} from "../controllers/auth.controller.js"
-import { validate } from "../middlewares/validator.middleware.js";
-import { userRegisthrationValidator } from "../validators/index.js";
 const router = Router();
 
-router.route("/register").post(userRegisthrationValidator() ,validate,registerUser)
+router.route("/").get(healthCheck);
 
 export default router;
